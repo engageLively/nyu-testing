7d31718ebf9775b087c180c1cdcf3cbfd9d76d25
Matt Hemmings <discount.yoyos@gmail.com>
2024-10-17 11:19:42 -0700

    Remove .ipynb_checkpoints files from tracking

--- postgres-container/.ipynb_checkpoints/install_libraries-checkpoint.sh
+++
@@ -1,4 +0,0 @@
-apt-get update
-apt-get install -y docker-compose curl
-curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
--- postgres-container/postgres/data/.ipynb_checkpoints/setup_db-checkpoint.sql
+++
@@ -1,4 +0,0 @@
-DROP TABLE world;
-CREATE TABLE world(id INT, word VARCHAR(255));
-COPY world(id, word) FROM './lines.csv' DELIMITER ',' CSV HEADER;
-SELECT * from world LIMIT 10;
