ae5274b86aff33f93a94b777877ba61809c712f0
Matt Hemmings <discount.yoyos@gmail.com>
2024-10-21 10:32:43 -0700

    Add connectivity tests to ensure communication is working

---
+++ docker-compose-4-containers/run_connectivity_tests.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# List of services to run the test script in
+services=("lind_lamp_plaintext" "lind_lamp_queries" "lind_lamp_mixed" "lind_nginx_static")
+
+# Loop through each service and run the test_connectivity.sh script inside the container
+for service in "${services[@]}"; do
+    echo "Running test_connectivity.sh in $service..."
+    
+    # Execute the script inside the container using docker-compose exec
+    docker-compose exec "$service" /usr/local/bin/test_connectivity.sh
+
+    echo "--------------------------"
+done
