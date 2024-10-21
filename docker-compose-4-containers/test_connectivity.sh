5b02c64720ca02a0980d9cfe63e3f878ba0ccced
Matt Hemmings <discount.yoyos@gmail.com>
2024-10-21 10:46:43 -0700

    add nginx container to test

--- docker-compose-4-containers/test_connectivity.sh
+++ docker-compose-4-containers/test_connectivity.sh
@@ -4,7 +4,7 @@
 current_container=$(hostname)
 
 # List of all containers to ping
-containers=("lind_lamp_plaintext" "lind_lamp_queries" "lind_lamp_mixed")
+containers=("lind_lamp_plaintext" "lind_lamp_queries" "lind_lamp_mixed" "lind_nginx_static")
 
 # Loop through each container and ping it, excluding the current one
 for container in "${containers[@]}"; do
